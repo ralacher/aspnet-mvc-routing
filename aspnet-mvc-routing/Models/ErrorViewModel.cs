c0e9263d62a47db2b2ee0b292bfd0975ccfd6185
Robert Lacher <robert.lacher@gmail.com>
2024-08-21 15:30:13 -0400

    initial commit

---
+++ aspnet-mvc-routing/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace aspnet_mvc_routing.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
